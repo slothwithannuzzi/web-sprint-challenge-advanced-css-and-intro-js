f3a60e5bbbcebe6dd4089170625740a9f6df9aaa
slothwithannuzzi <78941408+slothwithannuzzi@users.noreply.github.com>
2021-03-07 11:18:27 -0600

    completed task 1

--- index.js
+++ index.js
@@ -209,10 +209,11 @@ Practice accessing data above by console.log-ing following items:
 
 //(1) Name of the first artist (0th index) in the array
 
+console.log(artists[0].name);
 
 //(2) Bio of the third artist (2nd index) in the array 
 
-
+console.log(artists.bio)
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 (no function needed) 
