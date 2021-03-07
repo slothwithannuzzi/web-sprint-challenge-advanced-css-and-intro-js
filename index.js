176d44602c7aa137050ea635398b89afbb2ddd93
slothwithannuzzi <78941408+slothwithannuzzi@users.noreply.github.com>
2021-03-07 11:58:15 -0600

    completed task 5

--- index.js
+++ index.js
@@ -268,8 +268,9 @@ function get20s(arr){
  
  For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/){
-   /*Your Code Here*/
+ function removeArtist(arr, num){
+  arr.splice(num, num + 1);
+  return arr.length;
 }
    
 
